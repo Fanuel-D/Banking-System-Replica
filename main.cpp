425f7340bb999f6b0cdd2a49a8961e4d106fdbdc
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-06-09 19:09:04 +0300

    Some Revision

--- main.cpp
+++ main.cpp
@@ -15,24 +15,20 @@ int main(){
     string tempPassword = "";
     int userchoice;
     unordered_map<int, Account*> my_dict;
+    int user_num;
     
     while (true)
     {
 
         cout << " 1. Create an account " << endl;
         cout << " 2. Login" << endl;
-        cout << " 3. Deposit " << endl;
-        cout << " 4. Withdraw" << endl;
-        cout << " 5. Transfer " << endl;
         cout << " Choose the action to be performed : ";
         cin >> userchoice;
 
-        switch (userchoice)
-        {
-        case 1:
-        {
-            system("cls");
-            cout << "Enter Your Name ";
+        
+        if (userchoice == 1){
+             system("cls");
+            cout << "Enter Your Name: ";
             getline(cin.ignore(), tempname);
 
         begin:
@@ -58,12 +54,43 @@ int main(){
 
             cout << "This is your account number: " << account->get_accountNumber() << endl;
             cout << "This is your balance: " << account->get_balance() << endl;
+        }else{
+
+        start:
+            cout << "Enter Your account number: ";
+            cin >> user_num;
+            
+            int choice;
+            auto it = my_dict.find(user_num);
+            if (it != my_dict.end()) {
+            // Key found, use the iterator to access the value
+            Account* curr_account = it->second;
+            
+                cout << " 1. Deposit " << endl;
+                cout << " 2. Withdraw" << endl;
+                cout << " 3. Transfer " << endl;
+                cout << " Choose the action to be performed : ";
+                cin >> choice; 
+                    if (choice == 1){
+
+                    }else if (choice == 2){
+
+                    }else if (choice == 3){
+
+                    }
+                } else {
+                // Key not found
+                cout << "Key not found in the unordered_map." << endl;
+                goto start;
+                }
+
+        }
+        }
+        
+        return 0;
         }
 
 
     
 
-    return 0;
-}
-}
-}
+  
